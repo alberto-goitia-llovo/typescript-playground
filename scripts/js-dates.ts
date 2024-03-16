00017cd73baba7761d821983604fac1b414d2843
Alberto Goitia Llovo <alberto.goitia.ll@gmail.com>
2024-03-16 17:48:52 +0100

    init playground

---
+++ scripts/js-dates.ts
@@ -0,0 +1,11 @@
+const now = new Date();
+console.log("Current date and time:", now);
+console.log("Date as a string:", now.toDateString());
+console.log("Date as an ISO string:", now.toISOString());
+console.log("Date as a JSON string:", now.toJSON());
+console.log("Date as a localized date string:", now.toLocaleDateString());
+console.log("Date as a localized string:", now.toLocaleString());
+console.log("Date as a localized time string:", now.toLocaleTimeString());
+console.log("Date as a string:", now.toString());
+console.log("Date as a time string:", now.toTimeString());
+console.log("Date as a UTC string:", now.toUTCString());
---
+++ scripts/luxon.ts
@@ -0,0 +1,10 @@
+import { DateTime } from "luxon";
+const now = DateTime.now();
+console.log(now);
+console.log(now.toISO(), "toISO");
+console.log(now.toISODate(), "toISODate");
+console.log(now.toISOTime(), "toISOTime");
+console.log(now.toJSDate(), "toJSDate");
+console.log(now.toJSON(), "toJSON");
+console.log(now.toLocal(), "toLocal");
+console.log(now.toLocaleString(), "toLocaleString");
